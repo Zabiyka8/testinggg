c6dfd5d260a8e0570ffd43220a2b2a3d1ed496c4
zabiyka8 <Dplehov88@gmail.com>
2024-10-10 23:23:42 +0300

    weht

--- src/main/java/com/example/testing/model/Stock.java
+++ src/main/java/com/example/testing/model/Stock.java
@@ -1,2 +1,21 @@
-package com.example.testing.model;public class Stock {
+package com.example.testing.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Entity
+@Table(name = "stock_t")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer amount;
 }
